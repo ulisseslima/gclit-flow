a6636f581890c20f4f5571c6ef39e1bcc723a202
Ulisses Lima <ulisses@dvlcube.com>
2020-10-15 23:56:53 -0300

    changelog, repo link

---
+++ repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+# @installable
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
+ME=$(basename $MYSELF)
+
+source $MYDIR/env
+[[ -f $LOCAL_ENV ]] && source $LOCAL_ENV 
+source $MYDIR/log.sh
+source $MYDIR/db.sh
+
+google-chrome 'https://github.com/ulisseslima/gclit-flow'
