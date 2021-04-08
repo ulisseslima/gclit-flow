ec0ab8b16d152e3b46a81774471a05c085470a7a
Ulisses Lima <ulisses@dvlcube.com>
2021-04-08 16:41:31 -0300

    update installation

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+# @installable
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
+ME=$(basename $MYSELF)
+
+source $MYDIR/env
+[[ -f $LOCAL_ENV ]] && source $LOCAL_ENV 
+source $MYDIR/log.sh
+
+echo "updating gclit..."
+echo "$MYDIR"
+cd $MYDIR
+git pull
+
+$MYDIR/setup.sh
