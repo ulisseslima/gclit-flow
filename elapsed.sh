5f0c06ec6f69250805a2eb160804224bda433c82
Ulisses Lima <ulisses@dvlcube.com>
2021-04-08 22:39:29 -0300

    yesterday

--- elapsed.sh
+++ elapsed.sh
@@ -15,6 +15,7 @@ task_id="$1"
 info "total time worked (overall)"
 
 >&2 $MYDIR/psql.sh "select
+    (select coalesce(sum(elapsed), '0 hours') from executions where start between now()::date - interval '1 day' and now()::date) as yesterday,
     (select coalesce(sum(elapsed), '0 hours') from executions where start > now()::date) as today,
     (select coalesce(sum(elapsed), '0 hours') from executions where start > now()::date - interval '1 week') as last_week,
     (select coalesce(sum(elapsed), '0 hours') from executions where start > now()::date - interval '1 month') as last_month,
@@ -25,6 +26,7 @@ if [[ -n "$task_id" ]]; then
     info "total time worked (for task #${task_id})"
 
     >&2 $MYDIR/psql.sh "select
+        (select coalesce(sum(elapsed), '0 hours') from executions where task_id = $task_id and start between now()::date - interval '1 day' and now()::date) as yesterday,
         (select coalesce(sum(elapsed), '0 hours') from executions where task_id = $task_id and start > now()::date) as today,
         (select coalesce(sum(elapsed), '0 hours') from executions where task_id = $task_id and start > now()::date - interval '1 week') as last_week,
         (select coalesce(sum(elapsed), '0 hours') from executions where task_id = $task_id and start > now()::date - interval '1 month') as last_month,
